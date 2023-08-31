4e7b34e029246e0869f92390637daab193d01f09
Rahul Kumar Dewangan <rahuldewangan275@gmail.com>
2023-08-31 13:38:13 +0530

    commit 3

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -2,37 +2,20 @@ package com.driver.test;
 
 import com.driver.Application;
 import com.driver.DeliveryPartner;
+import com.driver.Order;
 import com.driver.OrderController;
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 
 import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
+
+import java.util.HashSet;
+import java.util.List;
 
 @SpringBootTest(classes = Application.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestCases {
-//    @Autowired
-//    OrderController orderController;
-//
-//    public void prepare(){
-//       orderController.addPartner(new DeliveryPartner("1"));
-//       orderController.addPartner(new DeliveryPartner("2"));
-//       orderController.addPartner(new DeliveryPartner("3"));
-//    }
-//    @Test
-//    public void addPartnerTest(){
-//        DeliveryPartner exp1 = new DeliveryPartner("1");
-//        DeliveryPartner exp2 = new DeliveryPartner("2");
-//
-//        ResponseEntity out1 = orderController.getPartnerById("1");
-//        ResponseEntity out2 = orderController.getPartnerById("2");
-//
-//
-
-
 
 }
